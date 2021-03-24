98cd3368934cc566fdb06077e7a801c571c5a062
petergardiner25 <50341968+petergardiner25@users.noreply.github.com>
2021-03-24 14:50:15 -0600

    stuffs

---
+++ PeterGardinerAssignment3/Models/MovieListContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PeterGardinerAssignment3.Models
+{
+    public class MovieListContext : DbContext
+    {
+        public MovieListContext (DbContextOptions<MovieListContext> options) : base (options)
+        {
+
+        }
+
+        public DbSet<MovieListContext> Movies { get; set; }
+    }
+}
